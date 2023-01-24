3c46a488ab3cd03cf6f8cd8c653cdaf6b5f79f28
nifty_afk <nifty_afk@yahoo.com>
2023-01-24 13:00:05 +0100

    sorting algorithm corrections

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,39 +1,33 @@
 #include "sort.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * bubble_sort - Sort by swapping largest items to end of array
- * @array: Array to be sorted
- * @size: Size of the array
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
+ *
+ *@array: array of ints
+ *@size: size of array
+ *
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t j;
-	int swapped;
-	size_t temp;
-	size_t tempsize;
+	int temp;
+	size_t i, k;
 
-	if (array == NULL)
+	if (!array || !size)
 		return;
-	tempsize = size;
-	swapped = 1;
-	while (swapped)
+
+	i = 0;
+	while (i < size)
 	{
-		swapped = 0;
-		j = 0;
-		while (j < tempsize - 1)
+		for (k = 0; k < size - 1; k++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[k] > array[k + 1])
 			{
-				temp = array[j + 1];
-				array[j + 1] = array[j];
-				array[j] = temp;
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 				print_array(array, size);
-				swapped = 1;
 			}
-			j++;
 		}
-		tempsize = j;
+		i++;
 	}
 }
